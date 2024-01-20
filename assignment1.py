4e64d02a6d4cd4df23e1d27b4b069ccd53e78929
paob02 <paulineb@student.chalmers.se>
2024-01-20 15:41:49 +0100

    idk

--- assignment1.py
+++ assignment1.py
@@ -1,1 +1,18 @@
-print("test")
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+
+df = pd.read_csv('swedish_population_by_year_and_sex_1860-2022.csv')
+
+children_list = []
+
+workforce_list = []
+
+elderly_list = []
+
+children_np = np.array(children_list)
+
+workforce_np = np.array(workforce_list)
+
+elderly_np = np.array(elderly_list)
