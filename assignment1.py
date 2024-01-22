d683894579df795de84cc613e9beb910005ca144
paob02 <paulineb@student.chalmers.se>
2024-01-22 17:06:44 +0100

    errör om att mina arrays inte är samma storlek även fast dom är det

--- assignment1.py
+++ assignment1.py
@@ -28,11 +28,11 @@ sum_of_children_rows = np.sum(modified_population_array2[children_rows_to_add], 
 sum_of_workforce_rows = np.sum(modified_population_array2[workforce_rows_to_add], axis=0)
 sum_of_elderly_rows = np.sum(modified_population_array2[elderly_rows_to_add], axis=0)
 
-print(sum_of_children_rows[0])
-print(sum_of_elderly_rows[0])
-print(sum_of_workforce_rows[0])
+#print(sum_of_children_rows[0])
+#print(sum_of_elderly_rows[0])
+#print(sum_of_workforce_rows[0])
 
-print(len(sum_of_children_rows))
+#print(len(sum_of_children_rows))
 
 
 sum_of_all = []
@@ -40,14 +40,19 @@ sum_of_all = []
 for i in range(len(sum_of_children_rows)):
     sum_of_all.append(100*(sum_of_children_rows[i] + sum_of_elderly_rows[i])/sum_of_workforce_rows)
 
-print(sum_of_all[0])
+#print(sum_of_all[0])
 
 year_array = np.arange(1860, 2023)
 
-print(year_array)
+#print(len(sum_of_all))
+#print(len(year_array))
+
+
+#print(year_array)
 
 
 plt.scatter(year_array, sum_of_all)
+
 
 # Adding labels and title
 plt.xlabel('Year')
