79864cfb5740de8dbba6e964fc339b229a55577a
paob02 <paulineb@student.chalmers.se>
2024-01-30 14:01:55 +0100

    börjat lite på ass2

--- assignment2.py
+++ assignment2.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
 
-file_path = 'kungalv_slutpriser.tar.gz'
+# Example: Fetching HTML content from a URL
+import requests
 
+url = 'kungalv_slutpriser.tar.gz'
+response = requests.get(url)
+html_content = response.text
+
+print(html_content)
