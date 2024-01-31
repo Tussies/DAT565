1a2d31c20239ffe24dffeb93f3edf66cb8ead148
paob02 <paulineb@student.chalmers.se>
2024-01-31 15:10:15 +0100

    fungerar lite

--- assignment2.py
+++ assignment2.py
@@ -5,12 +5,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 
-#How
-#tar_file = 'kungalv_slutpriser.tar.gz'
-#extract_folder = 'extracted_html'
-
-#with tarfile.open(tar_file, 'r') as tar:
-#    tar.extractall(extract_folder)
 
 html_map = 'kungalv_slutpriser/kungalv_slutpris_page_01.html'
 
@@ -20,14 +14,47 @@ with open(html_map, 'r') as file:
 
 soup = BeautifulSoup(html_content, 'html.parser')
 
-date_list = list()
+dates = list()
 for cell in soup.find_all('span', class_='hcl-label hcl-label--state hcl-label--sold-at'):
-    date_list.append(cell.text.strip())
+    dates.append(cell.text.strip())
 
-print(date_list)
+print(dates)
 
-#html_map.find('hcl-label hcl-label--state hcl-label--sold-at')
-#html_map.find_all('hcl-label hcl-label--state hcl-label--sold-at')
+soup.find('span', class_='row')
+
+import re
+element = soup.find(string=re.compile(r'.*1423,00.*'))
+
+table = element.parent.parent.parent.parent.parent
+table.dates
+
+table.find('span', class_='row')
+
+for cell in table.find('span', class_='row').select('span'):
+    print(cell.text.strip())
+
+values = list()
+for row in table.find_all('span', class_='row'):
+    values.append([cell.text.strip() for cell in row.select('td')])
+
+values = [list(map(lambda s: s.replace('\xa0','').replace(',','.'),row)) for row in values]
+values = [list(map(float,row[:3])) + [int(row[3])] + \
+          list(map(float,row[4:6])) + [int(row[6])] + [row[7]] for row in values]
+
+data = list()
+for (name,val) in zip(dates,values):
+    row = { 'Date' : date,
+            'Adress' : val[0],
+            'Location' : val[1],
+            'Boarea' : val[2],
+            'Biarea' : val[3],
+            'Rum' : val[4],
+            'Plot' : val[5],
+            'Closing price' : val[6]
+          }
+    data.append(row)
+data = pd.DataFrame(data)
+
 
 # date of sale class name = "hcl-label hcl-label--state hcl-label--sold-at"
 # Adress class name = "sold-property-listing__heading qa-selling-price-title hcl-card__title"
