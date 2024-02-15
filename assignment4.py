8ce903c52444e664391ad2e88241a3505bbb56a4
Tussies <86693581+Tussies@users.noreply.github.com>
2024-02-15 14:42:57 +0100

    correct until problem 2

--- assignment4.py
+++ assignment4.py
@@ -2,6 +2,9 @@ import csv
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
 
 def read_csv_file(file_path):
     data = []
@@ -18,21 +21,7 @@ header = life_expectancy[0]
 life_expectancy = life_expectancy[1:]
 life_expectancy_numeric = np.array([[np.nan if val == '' else float(val) for val in row[1:]] for row in life_expectancy], dtype=np.float64)
 
-X = life_expectancy_numeric[:, 1:-1]
-y = life_expectancy_numeric[:, -1]
+y = life_expectancy_numeric[:, -4]
+X = life_expectancy_numeric[:, :-4]
 
 LifeTrain, LifeTest, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
-
-columns = header[2:-4] + header[-3:]
-
-print("Shape of LifeTrain:", LifeTrain.shape)
-print("Number of columns specified:", len(columns))
-
-df_train = pd.DataFrame(data=np.hstack((LifeTrain, y_train.reshape(-1, 1))), columns=columns + ['Life Expectancy at Birth, both sexes (years)'])
-
-correlation_coefficients = df_train.corr()['Life Expectancy at Birth, both sexes (years)'][:-1]
-max_correlation_variable = correlation_coefficients.abs().idxmax()
-max_correlation_coefficient = correlation_coefficients.abs().max()
-
-print(f"The variable '{max_correlation_variable}' has the strongest linear relationship with the target variable.")
-print(f"Pearson correlation coefficient: {max_correlation_coefficient}")
