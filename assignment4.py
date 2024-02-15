e8886816489742b94e9fb21e1b124949d19d76a1
paob02 <paulineb@student.chalmers.se>
2024-02-15 09:45:49 +0100

    fel

--- assignment4.py
+++ assignment4.py
@@ -3,6 +3,12 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import r_regression
 
+from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+import matplotlib.pyplot as plt
+
+
 def read_csv_file(file_path):
     data = []
     with open(file_path, 'r') as file:
@@ -14,7 +20,9 @@ def read_csv_file(file_path):
 file_path = 'life_expectancy.csv'
 life_expectancy = read_csv_file(file_path)
 
+LifeTrain, LifeTest = train_test_split(life_expectancy, test_size=0.25, random_state=42)
 
-LifeTrain, LifeTest = train_test_split(X, test_size=0.25, random_state=42)
+print(LifeTest)
 
-print(train_test_split)
+plt.scatter(X, y)
+plt.show()
